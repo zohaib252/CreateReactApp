b4e91507f1aab053e9b01bcb7f881206b557613c
zohaib252 <Zulfiqarzohaib55@gmail.com>
2025-03-13 00:05:22 +0500

    Adding props

--- src/index.js
+++ src/index.js
@@ -73,10 +73,36 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozzarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza funghi"
+        ingredients="Tomato, mushrooms "
+        price={40}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        {" "}
+        <h3> {props.name}</h3>
+        <p> {props.ingredients} </p>
+        <span>{props.price + 5}</span>
+      </div>
+    </div>
   );
 }
 
@@ -97,15 +123,6 @@ function Footer() {
   );
 }
 
-function Pizza() {
-  return (
-    <div>
-      <img src={"pizzas/spinaci.jpg"} alt={"Pizza spinaci"} />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozzarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
